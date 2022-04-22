01857745bc3c768f85e359dde38fda15b3e9a340
abrahumlink <307887491@qq.com>
2022-04-22 13:16:12 +0800

    rm walle

--- walle/examples/echo.rs
+++
@@ -1,9 +0,0 @@
-use walle::{builtin::Echo, Plugins, Walle};
-use walle_core::AppConfig;
-
-#[tokio::main]
-async fn main() {
-    let plugins = Plugins::new().add_message_plugin(Echo::new());
-    let walle = Walle::new(AppConfig::default(), plugins);
-    walle.start().await.unwrap();
-}
--- walle/src/builtin/mod.rs
+++
@@ -1,8 +0,0 @@
-mod echo;
-pub use echo::*;
-mod rule;
-pub use rule::*;
-mod pre_handle;
-pub use pre_handle::*;
-mod matcher;
-pub use matcher::*;
