f36b8d73ebca3c3f4355f3b340658ba0f14ce2eb
abrahumlink <307887491@qq.com>
2022-05-30 15:52:19 +0800

    release 0.4.0-beta.3

--- src/lib.rs
+++ src/lib.rs
@@ -43,6 +43,6 @@ pub use handle::{ActionHandler, DefaultHandler};
 pub use hooks::*;
 pub use message::{Message, MessageAlt, MessageBuild, MessageSegment};
 pub use resp::{Resp, RespContent, Resps, StandardResps};
-pub use utils::{ExtendedMap, ExtendedMapExt, ExtendedValue, SelfId};
+pub use utils::{ColoredAlt, ExtendedMap, ExtendedMapExt, ExtendedValue, SelfId};
 
 pub use async_trait::async_trait;
