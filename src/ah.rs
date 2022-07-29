0fd68fd9489e48be6d2c541ababcb3b92c1f2174
abrahumlink <307887491@qq.com>
2022-07-29 14:51:12 +0800

    feat(handler): add JoinedHandler

--- src/ah.rs
+++ src/ah.rs
@@ -3,6 +3,7 @@ use std::sync::Arc;
 use async_trait::async_trait;
 
 use crate::error::WalleResult;
+use crate::util::SelfId;
 use crate::util::SelfIds;
 use crate::EventHandler;
 use crate::OneBot;
@@ -24,4 +25,79 @@ pub trait ActionHandler<E, A, R, const V: u8>: GetStatus + SelfIds + Sync {
 
 pub trait GetStatus {
     fn get_status(&self) -> crate::structs::Status;
+}
+
+pub struct JoinedHandler<H0, H1>(pub H0, pub H1);
+
+pub trait AHExt<E, A, R, const V: u8> {
+    fn join<AH1>(self, action_handler: AH1) -> JoinedHandler<Self, AH1>
+    where
+        Self: Sized,
+    {
+        JoinedHandler(self, action_handler)
+    }
+}
+
+impl<T: ActionHandler<E, A, R, V>, E, A, R, const V: u8> AHExt<E, A, R, V> for T {}
+
+#[async_trait]
+impl<H0, H1> SelfIds for JoinedHandler<H0, H1>
+where
+    H0: SelfIds + Send + Sync + 'static,
+    H1: SelfIds + Send + Sync + 'static,
+{
+    async fn self_ids(&self) -> Vec<String> {
+        let mut ids = self.0.self_ids().await;
+        ids.extend(self.1.self_ids().await);
+        ids
+    }
+}
+
+impl<H0, H1> GetStatus for JoinedHandler<H0, H1>
+//todo
+where
+    H0: GetStatus,
+{
+    fn get_status(&self) -> crate::structs::Status {
+        self.0.get_status()
+    }
+}
+
+#[async_trait]
+impl<AH0, AH1, E, A, R, const V: u8> ActionHandler<E, A, R, V> for JoinedHandler<AH0, AH1>
+where
+    AH0: ActionHandler<E, A, R, V> + Send + Sync + 'static,
+    AH0::Config: Send + Sync + 'static,
+    AH1: ActionHandler<E, A, R, V> + Send + Sync + 'static,
+    AH1::Config: Send + Sync + 'static,
+    A: SelfId + Send + Sync + 'static,
+    R: From<crate::resp::RespError>,
+{
+    type Config = (AH0::Config, AH1::Config);
+    async fn start<AH, EH>(
+        &self,
+        ob: &Arc<OneBot<AH, EH, V>>,
+        config: Self::Config,
+    ) -> WalleResult<Vec<tokio::task::JoinHandle<()>>>
+    where
+        AH: ActionHandler<E, A, R, V> + Send + Sync + 'static,
+        EH: EventHandler<E, A, R, V> + Send + Sync + 'static,
+    {
+        let mut joins = self.0.start(ob, config.0).await?;
+        joins.extend(self.1.start(ob, config.1).await?.into_iter());
+        Ok(joins)
+    }
+    async fn call(&self, action: A) -> WalleResult<R> {
+        if self.0.self_ids().await.contains(&action.self_id()) {
+            self.0.call(action).await
+        } else if self.1.self_ids().await.contains(&action.self_id()) {
+            self.1.call(action).await
+        } else {
+            Ok(crate::resp::resp_error::bad_request("bot not exist").into())
+        }
+    }
+    async fn shutdown(&self) {
+        self.0.shutdown().await;
+        self.1.shutdown().await;
+    }
 }
--- src/eh.rs
+++ src/eh.rs
@@ -20,3 +20,49 @@ pub trait EventHandler<E, A, R, const V: u8>: Sync {
     async fn call(&self, event: E) -> WalleResult<()>;
     async fn shutdown(&self) {}
 }
+
+use crate::ah::JoinedHandler;
+
+pub trait EHExt<E, A, R, const V: u8> {
+    fn join<EH1>(self, event_handler: EH1) -> JoinedHandler<Self, EH1>
+    where
+        Self: Sized,
+    {
+        JoinedHandler(self, event_handler)
+    }
+}
+
+impl<T: EventHandler<E, A, R, V>, E, A, R, const V: u8> EHExt<E, A, R, V> for T {}
+
+#[async_trait]
+impl<EH0, EH1, E, A, R, const V: u8> EventHandler<E, A, R, V> for JoinedHandler<EH0, EH1>
+where
+    EH0: EventHandler<E, A, R, V> + Send + Sync + 'static,
+    EH0::Config: Send + Sync + 'static,
+    EH1: EventHandler<E, A, R, V> + Send + Sync + 'static,
+    EH1::Config: Send + Sync + 'static,
+    E: Clone + Send + Sync + 'static,
+{
+    type Config = (EH0::Config, EH1::Config);
+    async fn start<AH, EH>(
+        &self,
+        ob: &Arc<OneBot<AH, EH, V>>,
+        config: Self::Config,
+    ) -> WalleResult<Vec<tokio::task::JoinHandle<()>>>
+    where
+        AH: ActionHandler<E, A, R, V> + Send + Sync + 'static,
+        EH: EventHandler<E, A, R, V> + Send + Sync + 'static,
+    {
+        let mut joins = self.0.start(ob, config.0).await?;
+        joins.extend(self.1.start(ob, config.1).await?.into_iter());
+        Ok(joins)
+    }
+    async fn call(&self, event: E) -> WalleResult<()> {
+        self.0.call(event.clone()).await?;
+        self.1.call(event).await
+    }
+    async fn shutdown(&self) {
+        self.0.shutdown().await;
+        self.1.shutdown().await;
+    }
+}
